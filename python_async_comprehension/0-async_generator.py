fdeebb05c333e8c190e26de94241df9ecc019003
GwenP88 <12777@holbertonstudents.com>
2026-04-08 11:41:28 +0200

    fix: use Generator type hint and gather with unpacking syntax

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ from random import uniform
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]: # type: ignore
     """Asynchronously yields 10 random floats between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
