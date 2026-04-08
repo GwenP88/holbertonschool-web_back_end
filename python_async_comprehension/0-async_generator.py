70728b7cfe18485f630da619c46da210c5ad2398
GwenP88 <12777@holbertonstudents.com>
2026-04-08 10:34:03 +0200

    Implement async_generator coroutine with AsyncGenerator typing and docstrings

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async generator coroutine module."""
+import random
+import asyncio
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Asynchronously yields 10 random floats between 0 and 10."""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
