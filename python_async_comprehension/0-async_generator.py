fce2b9c6c1ff01a57833161fbd5f3460a5f1696b
GwenP88 <12777@holbertonstudents.com>
2026-04-08 11:11:25 +0200

    style: use specific imports for random and time modules

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """Asynchronously yields 10 random floats between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield uniform(0, 10)
