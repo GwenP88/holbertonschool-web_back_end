956069b8f4c204a23d6c34ab58de6fc0150a110d
GwenP88 <12777@holbertonstudents.com>
2026-04-29 14:50:42 +0200

    Implement SkyHighBuilding class extending Building

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqtf, floors) {
+    super(sqtf);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return (`Evacuate slowly the ${this._floors} floors`);
+  }
+}
