e5e7c32d8bc661231b96c7d5ad39975692e12399
GwenP88 <12777@holbertonstudents.com>
2026-04-29 14:31:16 +0200

    Implement abstract Building class with evacuationWarningMessage enforcement

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    if (
+      new.target !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage
+    ) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
