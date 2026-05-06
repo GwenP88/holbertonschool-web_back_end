c9d56449a8afaac3968c58e2c20deae5a48d7b5e
GwenP88 <12777@holbertonstudents.com>
2026-05-06 15:07:03 +0200

    feat: implement getFullResponseFromAPI returning resolved or rejected promise based on boolean input

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+function getResponseFromAPI(success) {
+  const promise = new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return promise;
+}
+
+export default getResponseFromAPI;
