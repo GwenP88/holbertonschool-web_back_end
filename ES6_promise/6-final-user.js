fa9754713046b0ace1443eac826038aa91a608f8
GwenP88 <12777@holbertonstudents.com>
2026-05-06 16:47:03 +0200

    Process settled promises and normalize results

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,5 +4,11 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
-  return Promise.allSettled([user, photo]);
+  return Promise.allSettled([user, photo])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: String(result.reason) };
+    }));
 }
